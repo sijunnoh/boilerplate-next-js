d56e28175e85a4d5dfbca9288365f94c7fc7b9d6
sijunnoh23 <sijun.noh@avikus.ai>
2024-03-18 16:25:55 +0900

    chore: README 수정
    - README 파일 수정 및 기본 header 추가

---
+++ app/[locale]/_components/header.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header className="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
+      <div className="container flex h-14 max-w-screen-2xl items-center"></div>
+    </header>
+  )
+}
+export default Header
--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
+import Header from "./_components/header"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang={locale}>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
